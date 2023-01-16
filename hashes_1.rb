7eeedc5f9b494a04e19760d07b8b224571cf56d9
Crow Rising <miscbyrd@gmail.com>
2023-01-16 13:38:28 -0700

    complete hashes_1

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,29 +1,39 @@
 
-Activity:
+
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
 # file by entering the following command in your Terminal: 
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+# foods = {
+#   apples: 23, 
+#   grapes: 507,
+#   eggs: 48
+# }
 
-# Write code that prints all of the 'keys' of the foods variable 
-# you created above:
-# YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the foods variable 
-# you created above:
-# YOUR CODE HERE
+# # Write code that prints all of the 'keys' of the foods variable 
+# # you created above:
+# p foods.keys
 
-# Write code that prints the value of the second food of the foods variable 
-# you created above:
-# YOUR CODE HERE
 
-# Write code that adds a food to the foods hash. 
-# Then, print the updated hash:
-# YOUR CODE HERE
+# # Write code that prints all of the 'values' of the foods variable 
+# # you created above:
+# p foods.values
+
+# # Write code that prints the value of the second food of the foods variable 
+# # you created above:
+# p foods[:grapes]
+
+
+
+# # Write code that adds a food to the foods hash. 
+# # Then, print the updated hash:
+
+# foods[:bananas] = 8
+# p foods.keys
+# p foods.values
 
 
 #-------------------
@@ -37,16 +47,47 @@ p zoo
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+email = [
+  {
+  recipient: "Prof. Jenkins",
+  sender: "Crow Rising",
+  subject: "Questions about Mod1",
+  timestamp: "11:37am 16 January 2023",
+  message: "I am wondering when we will get to know who our partners will be for the first week of projects."
+  },
+  {
+    recipient: "Crow Rising",
+    sender: "Prof. Jenkins",
+    subject: "Response to Mod1 ??",
+    timestamp: "1:46pm 16 January 2023",
+    message: "We will be assigning partner in orientation on Monday."
+
+  },
+  {
+    recipient: "Grace Joh",
+    sender: "Crow Rising", 
+    subject: "Monday",
+    timestamp: "4:00pm 17 January 2023",
+    message: "I heard back from the porfessor about when we will be assigned partners."
+  }
+
+]
+
+# p email 
+# puts email
+p email[2]
+
 # Write code that prints your email hash to the terminal.
+# p email
 
 
 # Write code that prints all of the 'keys' of the email hash 
 # you created above:
-# YOUR CODE HERE
+# p email.keys
 
 # Write code that prints all of the 'values' of the email hash 
 # you created above:
-# YOUR CODE HERE
+# p email.values
 
 
 #-------------------
@@ -59,7 +100,7 @@ p zoo
 # Check out the following example of an array of Instagram posts:
 
 
-posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
+# posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 
 # An Array of Hashes is probably more realistic. Hashes are a data type that's great
@@ -67,25 +108,25 @@ posts = ["image at beach", "holiday party", "adorable puppy", "video of cute bab
 # that relate to each individual Instagram post:
 
 
-posts = [
-  {
-    image_src: "./images/beach.png",
-    caption: "At the beach with my besties",
-    timestamp: "4:37 PM August 13, 2019",
-    number_likes: 0,
-    comments: []
-  }, 
-  {
-    image_src: "./images/holiday-party.png",
-    caption: "What a great holiday party omg",
-    timestamp: "11:37 PM December 31, 2019",
-    number_likes: 13,
-    comments: []
-  }
-]
+# posts = [
+#   {
+#     image_src: "./images/beach.png",
+#     caption: "At the beach with my besties",
+#     timestamp: "4:37 PM August 13, 2019",
+#     number_likes: 0,
+#     comments: []
+#   }, 
+#   {
+#     image_src: "./images/holiday-party.png",
+#     caption: "What a great holiday party omg",
+#     timestamp: "11:37 PM December 31, 2019",
+#     number_likes: 13,
+#     comments: []
+#   }
+# ]
 
-p posts
-p posts[0]
+# p posts
+# p posts[0]
 
 
 # The code snippet above shows an Array with 2 elements. Each element in the Array is a 
